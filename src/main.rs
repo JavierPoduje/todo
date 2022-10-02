21dfcf8d2f2ad7f869ab41c15bdb3b8fc404df0b
JavierPoduje <javier.poduje@gmail.com>
2022-10-02 13:17:22 -0300

    feat: add to_do traits

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,10 @@
 mod to_do;
-
-use to_do::{to_do_factory, ItemTypes};
+use to_do::{to_do_factory, ItemTypes, structs::traits::create::Create};
 
 fn main() {
-    let to_do_item: Result<ItemTypes, &'static str> = to_do_factory("pending", "make");
+    let to_do_item: Result<ItemTypes, &'static str> = to_do_factory("pending", "washing");
     match to_do_item.unwrap() {
-        ItemTypes::Pending(item) => println!(
-            "it's a pending item with the title: {}",
-            item.super_struct.title
-        ),
+        ItemTypes::Pending(item) => item.create(&item.super_struct.title),
         ItemTypes::Done(item) => println!(
             "it's a done item with the title: {}",
             item.super_struct.title
