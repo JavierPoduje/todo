70f4a42d0d22ce6d9f4f63af2e3b69390c04f254
JavierPoduje <javier.poduje@gmail.com>
2022-10-02 12:55:00 -0300

    feat: add to_do's structs

--- src/to_do/structs/base.rs
+++ src/to_do/structs/base.rs
@@ -1,10 +1,10 @@
 pub struct Base {
     pub title: String,
-    pub status: STring,
+    pub status: String,
 }
 
 impl Base {
-    pub fn new(input_title,: &str, input_status: &str) -> Self {
+    pub fn new(input_title: &str, input_status: &str) -> Self {
         Self {
             title: input_title.to_string(),
             status: input_status.to_string(),
